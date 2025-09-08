ea5288d28454dfd7af3674619f47eaba5de177f0
Aldana-777 <jaldana818@unab.edu.co>
2025-09-08 08:57:52 -0500

    Add files via upload

---
+++ Quest 0 QZ.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+    class Main {
+    public static void main(String[] args) {
+        
+        String[] flor = {"amapola", "rosa", "tulipán", "orquídea"};
+
+        int flores = 7; 
+
+        for (String f : flor) {
+            System.out.println(f);
+        }
+    }
+}
